e88dc353fb24aeb18c96a7f72c879c42c7972592
Sayaka Ono <sayaka.ono.81@gmail.com>
2019-06-21 15:39:12 -0700

    implement API request

--- src/App.js
+++ src/App.js
@@ -1,7 +1,41 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-function App() {
-  return <div className="App" />;
+const query = `
+query {
+  title
+  description
 }
+`;
+
+const App = () => {
+  const [data, setData] = useState([]);
+
+  const getFilms = async query => {
+    try {
+      const response = await axios('https://ghibliapi.herokuapp.com/films');
+      console.log(response.data);
+      setData(response.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const renderList = films => {
+    return (
+      <ul>
+        {films.map(film => {
+          return <li key={film.id}>{film.title}</li>;
+        })}
+      </ul>
+    );
+  };
+
+  useEffect(() => {
+    getFilms(query);
+  }, []);
+
+  return <div className="App">{renderList(data)}</div>;
+};
 
 export default App;
