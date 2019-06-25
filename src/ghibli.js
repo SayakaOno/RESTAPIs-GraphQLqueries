c461527a7d2c35fa7a156547296d18ba5bd3f6a9
Sayaka Ono <sayaka.ono.81@gmail.com>
2019-06-24 19:44:35 -0700

    combine multiple REST API requests

--- src/ghibli.js
+++ src/ghibli.js
@@ -4,7 +4,9 @@ import { RestLink } from 'apollo-link-rest';
 import gql from 'graphql-tag';
 
 // setup your `RestLink` with your endpoint
-const restLink = new RestLink({ uri: 'https://ghibliapi.herokuapp.com/' });
+const restLink = new RestLink({
+  uri: 'https://ghibliapi.herokuapp.com/'
+});
 
 // setup your client
 const client = new ApolloClient({
@@ -13,39 +15,47 @@ const client = new ApolloClient({
 });
 
 const filmsQuery = gql`
-  query films {
+  query films($pathBuilder: any) {
     film @rest(type: "Film", path: "films") {
-      id
+      id @export(as: "url")
+      film @rest(pathBuilder: $pathBuilder, type: "Film") {
+        title
+        description
+      }
       title
       description
     }
   }
 `;
 
-const speciesQuery = gql`
-  query species {
-    species @rest(type: "Species", path: "species") {
-      name
-    }
-  }
-`;
+const getSpeciesPath = data => {
+  let path = data.exportVariables.url;
+  path = typeof path === 'object' ? path[0] : path;
+  return path.replace('https://ghibliapi.herokuapp.com/', '');
+};
 
 const peopleQuery = gql`
-  query people {
+  query people($pathBuilder: any) {
     people @rest(type: "People", path: "people") {
       name
-      species @rest(type: "Species", path: "species") {
+      species @export(as: "url")
+      species2 @rest(pathBuilder: $pathBuilder, type: "Species2") {
+        classification
         name
       }
-      films
+      films @export(as: "url")
+      film @rest(pathBuilder: $pathBuilder, type: "Film") {
+        title
+        release_date
+      }
     }
   }
 `;
 
 export default () => {
-  client.query({ query: peopleQuery }).then(response => {
-    // response.data.film.forEach(film => console.log(film.title));
-    response.data.people.forEach(person => console.log(person.name));
-    console.log(response.data);
-  });
+  client
+    .query({ query: peopleQuery, variables: { pathBuilder: getSpeciesPath } })
+    .then(response => {
+      console.log(response.data);
+    });
 };
